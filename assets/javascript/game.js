0ab1a3adaf7fd4db03ce8a0873c90575e1f84724
Jacob <jacob@Jacobs-MacBook-Air.local>
2016-10-27 18:13:26 -0400

    health var

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+var lukeHealth =100;
+var obiHealth =120;
+var sidHealth =150;
+var vaderHealth =180;
+
+$('#lukeHealth').text(lukeHealth);
+$('#obiHealth').text(obiHealth);
+$('#sidHealth').text(sidHealth);
+$('#vaderHealth').text(vaderHealth);
+
+$( "#one" ).append('<div ".chosenCharacter"></div>');
+
+})
